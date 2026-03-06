bbd5e81fbc33e1093265f3ff091936c4bcfd2627
Koder Kalash <kalashmashup@gmail.com>
2026-03-06 18:31:50 +0530

    added models and sighup contoller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,49 @@
+import bcrypt from "bcrypt";
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) return res.status(400).json({ message: "Invalid Credentials" });
+        if (name.trim() === "" || email.trim() === "" || password.trim() === "") return res.status(400).json({ message: "Invalid Credentials" });
+        if (password.length < 6) return res.status(400).json({ message: "Password must be at least 6 characters" });
+
+        const hashP = await bcrypt.hash(password, 10);
+        const user = await User.create({ name, email, password: hashP });
+
+        return res.status(201).json({
+            status: "success",
+            user
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Something went wrong.."
+        });
+    }
+};
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) return res.status(400).json({ message: "Invalid Credentials" });
+        if (email.trim() === "" || password.trim() === "") return res.status(400).json({ message: "Invalid Credentials" });
+        if (password.length < 6) return res.status(400).json({ message: "Password must be at least 6 characters" });
+
+        // const user = await User.findOne({ email });
+        // if (!user) return res.status(404).json({ message: "User not found" });
+
+        // const isMatch = await bcrypt.compare(password, user.password);
+        // if (!isMatch) return res.status(401).json({ message: "Invalid password" });
+
+        return res.status(200).json({
+            status: "success",
+            user
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Something went wrong.."
+        });
+    }
+};
---
+++ src/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+},{timeseries: true})
+
+const User = mongoose.Model("User", userSchema)
+
+export default User;
