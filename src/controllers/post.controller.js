490e257ee843417fb88986f803e8e0981b90a05b
Koder Kalash <kalashmashup@gmail.com>
2026-03-13 12:57:59 +0530

    added delete and update controllers

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -28,8 +28,8 @@ export const createPost = async (req, res) => {
 }
 
 export const readAllPost = async (req, res) => {
-    const posts = (await Post.find()).sort({createdAt: -1})
     try {
+        const posts = (await Post.find()).sort({ createdAt: -1 })
         return res.status(200).json({
             status: "success",
             posts
@@ -42,18 +42,18 @@ export const readAllPost = async (req, res) => {
     }
 }
 
-export const readPost = async(req,res) => {
+export const readPost = async (req, res) => {
     try {
         const id = req.params.id
-        if(!id) return res.status(400).json({
+        if (!id) return res.status(400).json({
             message: "Invalid Input"
         });
-        if(typeof id !== "string") res.status(400).json({
+        if (typeof id !== "string") res.status(400).json({
             message: "Invalid Input"
         });
 
-        const post = Post.findById(id);
-        if(!post) res.status(404).json({
+        const post = await Post.findById(id);
+        if (!post) res.status(404).json({
             message: "No Post found"
         });
 
@@ -70,11 +70,77 @@ export const readPost = async(req,res) => {
     }
 }
 
-export const updatePost = async (req,res) => {
+export const updatePost = async (req, res) => {
     /*
-    -> Take out the token from the header for authzn and updates from the body
     -> Find user and check whether the user exists and then check whether they are the original author
     -> If they are: update, else throw error
     -> return response
      */
+    const id = req.params.id
+    const user = req.user._id
+    if (!id) return res.status(400).json({
+        message: "Bad request"
+    });
+    if (typeof id !== "number") return res.status(400).json({
+        message: "Bad request"
+    });
+    if (!user) return res.status(400).json({
+        message: "Bad request"
+    });
+
+    const post = await Post.findById(id)
+    const isAuthor = user.toString() === post.author.toString()
+
+    if(!isAuthor) return res.status(403).json({
+        message: "Not Authorized"
+    });
+
+    const updatedPost =  await post.updateOne({
+        title: title,
+        content : content,
+    })//something here -> no shit
+
+    return res.status(200).json({
+        message: "success",
+        post : updatedPost
+    })
+    
+}
+
+export const deletePost = async (req, res) => {
+    try {
+        const id = req.params.id
+        const user = req.user._id
+
+        if (!user) return res.status(403).json({
+            message: "Not Authorized"
+        });
+        if (!id) return res.status(400).json({
+            message: "Bad request"
+        });
+        if (typeof id !== "number") return res.status(400).json({
+            message: "Bad request"
+        });
+
+        const post = await Post.findById(id);
+        const isAuthor = user.toString() === post.author.toString();
+        if (!isAuthor) return res.status(403).json({
+            message: "Not Authorized"
+        })
+
+        if (!post) return res.status(404).json({
+            message: "Post does not exist"
+        });
+
+        Post.deleteOne({ post })
+
+        return res.status(200).json({
+            message: "success"
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Something went wrong.."
+        });
+    }
 }
