c85b42d138a586e1d55800843f482d1e15fd904b
Koder Kalash <kalashmashup@gmail.com>
2026-03-15 18:40:09 +0530

    fixed protect middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,26 +1,27 @@
-
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
-
-//not me
 export const protect = async (req, res, next) => {
     try {
-        const authHeader = req.headers.authorization;
+        const header = req.headers.authorization;
 
-        if (!authHeader || !authHeader.startsWith("Bearer ")) {
-            return res.status(401).json({ message: "Not authorized" });
+        if (!header || !header.startsWith("Bearer ")) {
+            return res.status(401).json({ message: "Unauthorized" });
         }
 
-        const token = authHeader.split(" ")[1];
+        const token = header.split(" ")[1];
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         const user = await User.findById(decoded.userId).select("-password");
         if (!user) {
-            return res.status(401).json({ message: "Not authorized" });
+            return res.status(401).json({ message: "Unauthorized" });
         }
 
-        req.user = user;
+        req.user = user; //imp
         next();
     } catch (error) {
         return res.status(401).json({ message: "Invalid or expired token" });
